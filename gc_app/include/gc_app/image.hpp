b3d614679e3a0835d0eff653c6741027b319cd2d
Stepan Orlov <majorsteve@mail.ru>
2024-09-20 09:35:21 +0100

    gc_app: Add Size, Image, RectView.

---
+++ gc_app/include/gc_app/image.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "gc_app/types.hpp"
+
+namespace gc_app {
+
+template <typename T>
+struct Size final
+{
+    T width;
+    T height;
+};
+
+template <typename T>
+constexpr inline auto fields_of(Size<T>& x)
+    -> std::tuple<T&, T&>
+{ return { x.width, x.height }; }
+
+template <typename T>
+constexpr inline auto fields_of(const Size<T>& x)
+    -> std::tuple<const T&, const T&>
+{ return { x.width, x.height }; }
+
+template <typename T>
+inline auto fields_of(Size<T>&& x) = delete;
+
+template <typename T>
+constexpr inline auto tuple_tag_of(common::Type_Tag<Size<T>>)
+    -> common::Type_Tag<std::tuple<T, T>>
+{ return {}; }
+
+template <typename T>
+constexpr inline auto field_names_of(common::Type_Tag<Size<T>>)
+    -> std::array<std::string_view, 2>
+{ return { "width", "height" }; }
+
+using UintSize = Size<Uint>;
+
+// ---
+
+inline auto rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xff)
+    -> uint32_t
+{
+    return
+          (uint32_t{a} << 24)
+        |  uint32_t{r}
+        | (uint32_t{g} << 8)
+        | (uint32_t{b} << 16);
+}
+
+inline auto rgba(uint32_t rgb, uint8_t a = 0xff)
+    -> uint32_t
+{ return (uint32_t{a} << 24) | (rgb & ~0xff); }
+
+// ---
+
+struct Image final
+{
+    UintSize                size;
+    std::vector<uint32_t>   data;
+};
+
+} // namespace gc_app
+
+GC_REGISTER_CUSTOM_TYPE(gc_app::Image, 1);
---
+++ gc_app/src/rect_view.cpp
@@ -0,0 +1,57 @@
+#include "gc_app/rect_view.hpp"
+
+#include "gc_app/image.hpp"
+
+
+namespace gc_app {
+
+auto RectView::input_count() const
+    -> uint32_t
+{ return 2; }
+
+auto RectView::output_count() const
+    -> uint32_t
+{ return 1; }
+
+auto RectView::default_inputs(gc::ValueSpan result) const
+    -> void
+{
+    assert(result.size() == 2);
+    result[0] = UintSize(100, 100);
+    result[1] = UintVec(10000, 1);
+}
+
+auto RectView::compute_outputs(
+        gc::ValueSpan result,
+        gc::ConstValueSpan inputs) const
+    -> void
+{
+    assert(inputs.size() == 2);
+    assert(result.size() == 1);
+    const auto& size = inputs[0].as<UintSize>();
+    const auto& seq = inputs[1].as<UintVec>();
+    auto image = Image
+    {
+        .size = size,
+        .data = UintVec(size.width * size.height, rgba(0, 0))
+    };
+
+    auto N = *std::max_element(seq.begin(), seq.end());
+    auto d = 0xff / N;
+
+    auto n = std::max(image.data.size(), seq.size());
+    for(size_t index=0; index<n; ++index)
+    {
+        auto value = seq[index];
+        if (value != 0)
+        {
+            // *pixel = rgba(0xff, 0xff, 0xff);
+            auto v = value*d;
+            image.data[index] = rgba(0xff, 0xff-v, 0xff-v);
+        }
+    }
+
+    result[0] = std::move(image);
+}
+
+} // namespace gc_app
