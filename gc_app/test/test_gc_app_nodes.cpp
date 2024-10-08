8c846aefc198f80ab64cacc518ddb1647810eaee
Stepan Orlov <majorsteve@mail.ru>
2024-10-08 08:26:34 +0100

    gc_app (test): Test port names.

--- gc_app/test/test_gc_app_nodes.cpp
+++ gc_app/test/test_gc_app_nodes.cpp
@@ -17,6 +17,12 @@ TEST(GcApp, EratosthenesSieve)
     ASSERT_EQ(node->input_count(), 1);
     ASSERT_EQ(node->output_count(), 1);
 
+    ASSERT_EQ(node->input_names().size(), 1);
+    ASSERT_EQ(node->input_names()[0], "count");
+
+    ASSERT_EQ(node->output_names().size(), 1);
+    ASSERT_EQ(node->output_names()[0], "sequence");
+
     gc::ValueVec inputs(1);
     gc::ValueVec outputs(1);
 
@@ -44,6 +50,12 @@ TEST(GcApp, TestSequence)
 
     ASSERT_EQ(node->input_count(), 1);
     ASSERT_EQ(node->output_count(), 1);
+
+    ASSERT_EQ(node->input_names().size(), 1);
+    ASSERT_EQ(node->input_names()[0], "count");
+
+    ASSERT_EQ(node->output_names().size(), 1);
+    ASSERT_EQ(node->output_names()[0], "sequence");
 
     gc::ValueVec inputs(1);
     gc::ValueVec outputs(1);
@@ -73,6 +85,11 @@ TEST(GcApp, SourceParam)
     ASSERT_EQ(node->input_count(), 0);
     ASSERT_EQ(node->output_count(), 0);
 
+    ASSERT_EQ(node->input_names().size(), 0);
+    ASSERT_EQ(node->output_names().size(), 0);
+
+    // ---
+
     gc::ValueVec inputs = { 12, 3.4 };
 
     auto check = [&](gc::ConstValueSpan s)
@@ -84,7 +101,16 @@ TEST(GcApp, SourceParam)
 
     auto* param = InputParameters::get(node.get());
     param->set_inputs(inputs);
+    ASSERT_EQ(node->input_count(), 0);
     ASSERT_EQ(node->output_count(), 2);
+
+    ASSERT_EQ(node->input_names().size(), 0);
+
+    ASSERT_EQ(node->output_names().size(), 2);
+    ASSERT_EQ(node->output_names()[0], "out_0");
+    ASSERT_EQ(node->output_names()[1], "out_1");
+
+    // ---
 
     gc::ValueVec outputs(2);
     node->compute_outputs(outputs, {});
@@ -101,6 +127,13 @@ TEST(GcApp, Multiply)
 
     ASSERT_EQ(node->input_count(), 2);
     ASSERT_EQ(node->output_count(), 1);
+
+    ASSERT_EQ(node->input_names().size(), 2);
+    ASSERT_EQ(node->input_names()[0], "lhs");
+    ASSERT_EQ(node->input_names()[1], "rhs");
+
+    ASSERT_EQ(node->output_names().size(), 1);
+    ASSERT_EQ(node->output_names()[0], "product");
 
     auto check =
         [&]<typename T>(T a, T b)
@@ -122,6 +155,13 @@ TEST(GcApp, Project)
 
     ASSERT_EQ(node->input_count(), 2);
     ASSERT_EQ(node->output_count(), 1);
+
+    ASSERT_EQ(node->input_names().size(), 2);
+    ASSERT_EQ(node->input_names()[0], "value");
+    ASSERT_EQ(node->input_names()[1], "path");
+
+    ASSERT_EQ(node->output_names().size(), 1);
+    ASSERT_EQ(node->output_names()[0], "projection");
 
     auto check =
         [&]<typename T, typename P>(T value, gc::ValuePath path, P projection)
