3e5f93a9071d5c4121167361e6878a4e5838df40
Stepan Orlov <majorsteve@mail.ru>
2024-10-21 22:24:58 +0100

    gc_app: Use `GCLIB_STRUCT_TYPE` macro.

--- gc_app/include/gc_app/image.hpp
+++ gc_app/include/gc_app/image.hpp
@@ -3,6 +3,8 @@
 #include "gc_app/color.hpp"
 #include "gc_app/types.hpp"
 
+#include "gc/struct_type_macro.hpp"
+
 namespace gc_app {
 
 template <typename T>
@@ -12,30 +14,10 @@ struct Size final
     T height;
 };
 
-template <typename T>
-constexpr inline auto fields_of(Size<T>& x)
-    -> std::tuple<T&, T&>
-{ return { x.width, x.height }; }
-
-template <typename T>
-constexpr inline auto fields_of(const Size<T>& x)
-    -> std::tuple<const T&, const T&>
-{ return { x.width, x.height }; }
-
-template <typename T>
-inline auto fields_of(Size<T>&& x) = delete;
-
-template <typename T>
-constexpr inline auto tuple_tag_of(common::Type_Tag<Size<T>>)
-    -> common::Type_Tag<std::tuple<T, T>>
-{ return {}; }
-
-template <typename T>
-constexpr inline auto field_names_of(common::Type_Tag<Size<T>>)
-    -> std::array<std::string_view, 2>
-{ return { "width", "height" }; }
-
 using UintSize = Size<Uint>;
+
+GCLIB_STRUCT_TYPE(UintSize, width, height);
+
 
 struct Image final
 {
--- gc_app/include/gc_app/palette.hpp
+++ gc_app/include/gc_app/palette.hpp
@@ -2,6 +2,8 @@
 
 #include "gc_app/color.hpp"
 
+#include "gc/struct_type_macro.hpp"
+
 #include "common/type.hpp"
 
 #include <array>
@@ -21,23 +23,7 @@ struct IndexedPalette
     Color overflow_color;
 };
 
-constexpr inline auto fields_of(IndexedPalette& x)
-    -> std::tuple<IndexedColorMap&, Color&>
-{ return { x.color_map, x.overflow_color }; }
-
-constexpr inline auto fields_of(const IndexedPalette& x)
-    -> std::tuple<const IndexedColorMap&, const Color&>
-{ return { x.color_map, x.overflow_color }; }
-
-inline auto fields_of(IndexedPalette&& x) = delete;
-
-constexpr inline auto tuple_tag_of(common::Type_Tag<IndexedPalette>)
-    -> common::Type_Tag<std::tuple<IndexedColorMap, Color>>
-{ return {}; }
-
-constexpr inline auto field_names_of(common::Type_Tag<IndexedPalette>)
-    -> std::array<std::string_view, 2>
-{ return { "color_map", "overflow_color" }; }
+GCLIB_STRUCT_TYPE(IndexedPalette, color_map, overflow_color);
 
 
 auto map_color(const IndexedPalette& palette, Color value)
