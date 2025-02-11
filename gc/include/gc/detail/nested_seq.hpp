0e7749f235a8271c21562a4e59b86ed0147b7863
Stepan Orlov <majorsteve@mail.ru>
2025-02-11 23:15:49 +0000

    gc (detail): Implement nested sequences with an ability to test a stateful predicate.

---
+++ gc/include/gc/detail/nested_seq.hpp
@@ -0,0 +1,160 @@
+#pragma once
+
+#include "common/throw.hpp"
+
+#include <cassert>
+#include <concepts>
+#include <span>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+
+namespace gc::detail {
+
+enum class SeqType
+{
+    AllInOrder,
+    Every
+};
+
+struct BeginSeqMark
+{
+    SeqType type;
+};
+
+struct EndSeqMark {};
+
+template <typename T>
+using NestedSequenceItem = std::variant<BeginSeqMark, EndSeqMark, T>;
+
+template <typename T>
+using NestedSequence = std::vector<NestedSequenceItem<T>>;
+
+template <typename T>
+using NestedSequenceView = std::span<const NestedSequenceItem<T>>;
+
+template <typename P, typename T>
+concept NestedSequenceTest =
+    requires(P p, T item)
+    {
+        requires std::is_copy_assignable_v<P>;
+        requires std::is_copy_constructible_v<P>;
+        { p(item) } -> std::same_as<bool>;
+    };
+
+template <typename T, NestedSequenceTest<T> P>
+auto test_sequence_impl(NestedSequenceView<T> avail, P& p)
+    -> std::pair<size_t, bool>
+{
+    if(avail.size() <= 1)
+        common::throw_<std::invalid_argument>(
+            "test_sequence() expects an input sequence "
+            "of at least two elements");
+    if(!std::holds_alternative<BeginSeqMark>(avail.front()))
+        common::throw_<std::invalid_argument>(
+            "test_sequence() expects an input sequence "
+            "starting with a BeginSeqMark");
+    auto type = std::get<BeginSeqMark>(avail.front()).type;
+    size_t i = 1;
+    size_t n = avail.size();
+    auto p_backup = p;
+    while (i < n)
+    {
+        const auto& item = avail[i];
+        if (std::holds_alternative<BeginSeqMark>(item))
+        {
+            auto [nsub, ok] = test_sequence_impl(avail.subspan(i), p);
+            if (!ok)
+                return { i + nsub, false };
+            i += nsub;
+        }
+        else if (std::holds_alternative<EndSeqMark>(item))
+            return { i+1, true };
+        else
+        {
+            assert(std::holds_alternative<T>(item));
+            const auto& v = std::get<T>(item);
+            ++i;
+            if (!p(v))
+                return { i, false };
+        }
+        if (i < n && type == SeqType::Every)
+            p = p_backup;
+    }
+
+    common::throw_<std::invalid_argument>(
+        "test_sequence() expects an input sequence "
+        "ending with an EndSeqMark");
+}
+
+template <typename T, NestedSequenceTest<T> P>
+auto test_sequence(NestedSequenceView<T> seq, P p, P& final_state)
+    -> bool
+{
+    final_state = p;
+    while (true)
+    {
+        auto [n, ok] = test_sequence_impl(seq, final_state);
+        if (!ok)
+        {
+            final_state = p;
+            return false;
+        }
+        if (n == seq.size())
+            return true;
+        seq = seq.subspan(n);
+    }
+}
+
+// TODO
+//      There actually may be many final states, depending on
+//      which path is chosen in subsequences of type `SeqType::Every`.
+//
+//      Also, we lack subsequences of type `SeqType::Optional`; having these
+//      would increase the number of possible final states, too.
+//
+//      At the moment, `final_state` corresponds to the last item of each
+//      non-empty `SeqType::Every` subsequence.
+template <typename T, NestedSequenceTest<T> P>
+auto test_sequence(const NestedSequence<T>& seq, P p, P& final_state)
+    -> bool
+{
+    auto view = NestedSequenceView<T>{ seq };
+    return test_sequence(view, std::move(p), final_state);
+}
+
+template <typename T, NestedSequenceTest<T> P>
+auto test_sequence(NestedSequenceView<T> seq, P p)
+    -> bool
+{
+    auto final_state = p;
+    return test_sequence(seq, std::move(p), final_state);
+}
+
+template <typename T, NestedSequenceTest<T> P>
+auto test_sequence(const NestedSequence<T>& seq, P p)
+    -> bool
+{
+    auto final_state = p;
+    return test_sequence(seq, std::move(p), final_state);
+}
+
+
+template <typename T>
+class ScopedNestedSequence
+{
+public:
+    using Seq = NestedSequence<T>;
+    ScopedNestedSequence(Seq& seq, SeqType type):
+        seq_{ seq }
+    { seq_.emplace_back(BeginSeqMark{type}); }
+
+    ~ScopedNestedSequence()
+    { seq_.emplace_back(EndSeqMark{}); }
+
+private:
+    Seq& seq_;
+};
+
+} // namespace gc::detail
---
+++ gc/test/test_nested_seq.cpp
@@ -0,0 +1,100 @@
+#include "gc/detail/nested_seq.hpp"
+
+#include <gtest/gtest.h>
+
+
+namespace {
+
+struct Even final
+{
+    auto operator()(unsigned int x) -> bool
+    { return (x & 1) == 0; }
+};
+
+struct Ascending final
+{
+    unsigned int state_{};
+
+    auto operator()(unsigned int x) -> bool
+    {
+        if (x <= state_)
+            return false;
+        state_ = x;
+        return true;
+    }
+};
+
+} // anonymous namespace
+
+
+TEST(Gc, NestedSeq)
+{
+    using Seq = gc::detail::NestedSequence<unsigned int>;
+    using ScopedSeq = gc::detail::ScopedNestedSequence<unsigned int>;
+    using gc::detail::SeqType;
+
+    constexpr auto beg_all = gc::detail::BeginSeqMark{ SeqType::AllInOrder };
+    constexpr auto beg_every = gc::detail::BeginSeqMark{ SeqType::Every };
+    constexpr auto end = gc::detail::EndSeqMark{};
+
+    auto test_seq = [](Seq seq, auto pred, bool expected)
+    {
+        auto actual = gc::detail::test_sequence(seq, pred);
+        EXPECT_EQ(actual, expected);
+    };
+
+    auto even = Even{};
+
+    test_seq({ beg_all, end }, even, true);
+    test_seq({ beg_every, end }, even, true);
+    test_seq({ beg_all, 2u, end }, even, true);
+    test_seq({ beg_all, 2u, 4u, 0u, end }, even, true);
+    test_seq({ beg_all, 2u, 4u, 7u, end }, even, false);
+    test_seq({ beg_all, 2u, 4u, beg_every, 8u, 4u, end, end }, even, true);
+    test_seq({ beg_all, 1u, end }, even, false);
+    test_seq({ beg_all, 2u, end, beg_all, 4u, 7u, end }, even, false);
+    test_seq({ beg_all, 2u, end, beg_all, 4u, 8u, end }, even, true);
+
+    EXPECT_THROW(
+        test_seq({ beg_all, 2u }, even, true),
+        std::invalid_argument);
+    EXPECT_THROW(
+        test_seq({ 2u, end }, even, true),
+        std::invalid_argument);
+    EXPECT_THROW(
+        test_seq({}, even, true),
+        std::invalid_argument);
+    EXPECT_THROW(
+        test_seq({ beg_all, 2u, 4u, beg_every, 8u, 4u, end }, even, true),
+        std::invalid_argument);
+
+
+    auto ascending = Ascending{};
+
+    test_seq({ beg_all, 2u, 4u, 0u, end }, ascending, false);
+    test_seq({ beg_all, 2u, 4u, 5u, end }, ascending, true);
+    test_seq({ beg_every, 3u, 2u, 1u, end }, ascending, true);
+    test_seq({ beg_all, 3u, 2u, 1u, end }, ascending, false);
+    test_seq({ beg_every,
+               beg_all, 2u, 4u, 5u, end,
+               beg_all, 3u, 4u, 6u, end,
+               end }, ascending, true);
+    test_seq({ beg_all,
+               beg_all, 2u, 4u, 5u, end,
+               beg_all, 3u, 4u, 6u, end,
+               end }, ascending, false);
+    test_seq({ beg_all,
+               1u,
+               beg_every,
+               beg_all, 2u, 4u, 5u, end,
+               beg_all, 3u, 4u, 6u, end,
+               end,
+               end }, ascending, true);
+    test_seq({ beg_all,
+               2u,
+               beg_every,
+               beg_all, 2u, 4u, 5u, end,
+               beg_all, 3u, 4u, 6u, end,
+               end,
+               end }, ascending, false);
+}
