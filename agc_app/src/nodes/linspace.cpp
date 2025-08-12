635f2b59fb6549f488515b624fa9a698bac58337
Stepan Orlov <majorsteve@mail.ru>
2025-08-12 21:41:11 +0100

    agc_app: Fix header file to include by LinSpace generated code.

--- agc_app/src/nodes/linspace.cpp
+++ agc_app/src/nodes/linspace.cpp
@@ -78,7 +78,7 @@ public:
 
         auto linspace_alg_header =
             s(a::HeaderFile{
-                .name = "agc_app_rt/alg/linspace.hpp",
+                .name = "agc_app_rt/nodes/linspace.hpp",
                 .lib = lib });
 
         auto iter_init_func =
