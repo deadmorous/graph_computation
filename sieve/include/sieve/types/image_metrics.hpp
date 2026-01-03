c0f9f9f124f5bd4f18a31dff1e73ff76c6796a1e
Stepan Orlov <majorsteve@mail.ru>
2026-01-03 20:22:49 +0000

    sieve: Add ImageMetric enum and ImageMetricSet flag set types.

--- sieve/include/sieve/types/image_metrics.hpp
+++ sieve/include/sieve/types/image_metrics.hpp
@@ -3,19 +3,31 @@
  *
  * TODO: More documentation here
  *
- * Copyright (C) 2025 MPK Software, St.-Petersburg, Russia
+ * Copyright (C) 2025-2026 MPK Software, St.-Petersburg, Russia
  *
  * @author Stepan Orlov <majorsteve@mail.ru>
  */
 
 #pragma once
 
+#include "common/enum_flags.hpp"
 #include "common/struct_type_macro.hpp"
 
+#include "gc/type.hpp"
+
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
 namespace sieve {
+
+enum class ImageMetric : uint8_t
+{
+    StateHistogram,
+    EdgeHistogram
+};
+
+using ImageMetricSet = common::EnumFlags<ImageMetric>;
 
 struct ImageMetrics
 {
@@ -27,3 +39,5 @@ GCLIB_STRUCT_TYPE(ImageMetrics, histogram, edge_histogram);
 auto operator<<(std::ostream&, const ImageMetrics&) -> std::ostream&;
 
 } // namespace sieve
+
+GCLIB_REGISTER_ENUM_TYPE(sieve::ImageMetric, 1);
