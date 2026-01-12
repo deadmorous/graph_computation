00e9a776c0014d3d250f7188963f1ce2bebeb7d3
Stepan Orlov <majorsteve@mail.ru>
2026-01-12 08:58:06 +0000

    gc_visual: Implement time series image metric plot.

--- gc_visual/src/widgets/image_metrics_view.cpp
+++ gc_visual/src/widgets/image_metrics_view.cpp
@@ -19,7 +19,9 @@
 #include "gc/value.hpp"
 
 #include <QPainter>
+#include <QPainterPath>
 
+#include <algorithm>
 #include <deque>
 
 
@@ -83,9 +85,16 @@ auto ensure_palette(std::optional<gc_app::IndexedPalette>& palette,
     return pal;
 }
 
+enum class PlotMode : uint8_t
+{
+    HistogramHistory,
+    TimeSeries
+};
+
 struct MetricViewData
 {
     std::vector<double> sieve::ImageMetrics::* metric;
+    PlotMode plot_mode;
     std::optional<gc_app::IndexedPalette> ImageMetricsView::Storage::* palette;
     QColor (*default_palette_color)(int index, int state_count);
     bool strict_palette_match;
@@ -101,6 +110,7 @@ auto metric_view_data(sieve::ImageMetric type) -> MetricViewData
     case sieve::ImageMetric::StateHistogram:
         return {
             .metric = &sieve::ImageMetrics::histogram,
+            .plot_mode = PlotMode::HistogramHistory,
             .palette = &ImageMetricsView::Storage::state_palette,
             .default_palette_color = default_hue_color,
             .strict_palette_match = false,
@@ -111,6 +121,7 @@ auto metric_view_data(sieve::ImageMetric type) -> MetricViewData
     case sieve::ImageMetric::EdgeHistogram:
         return {
             .metric = &sieve::ImageMetrics::edge_histogram,
+            .plot_mode = PlotMode::HistogramHistory,
             .palette = &ImageMetricsView::Storage::edge_palette,
             .default_palette_color = default_alternating_color,
             .strict_palette_match = true,
@@ -118,17 +129,27 @@ auto metric_view_data(sieve::ImageMetric type) -> MetricViewData
             .y_label = "diff. fraction",
             .title = "Edge histogram"
         };
+    case sieve::ImageMetric::PlateauAvgSize:
+        return {
+            .metric = &sieve::ImageMetrics::plateau_avg_size,
+            .plot_mode = PlotMode::TimeSeries,
+            .palette = &ImageMetricsView::Storage::state_palette,
+            .default_palette_color = default_hue_color,
+            .strict_palette_match = false,
+            .x_label = "generation",
+            .y_label = "plateau size",
+            .title = "Average plateau size"
+        };
     }
     __builtin_unreachable();
 }
 
-auto plot_metric(ImageMetricsView::Storage& s,
-                 const QRect& rect,
-                 QPainter& painter)
+auto plot_histogram_metric(const MetricViewData& d,
+                           ImageMetricsView::Storage& s,
+                           const QRect& rect,
+                           QPainter& painter)
     -> void
 {
-    auto d = metric_view_data(s.type);
-
     auto generation_count = s.buf.size();
 
     if (generation_count == 0)
@@ -216,6 +237,126 @@ auto plot_metric(ImageMetricsView::Storage& s,
     }
 
     axes_painter.draw(painter);
+}
+
+auto plot_time_series_metric(const MetricViewData& d,
+                             ImageMetricsView::Storage& s,
+                             const QRect& rect,
+                             QPainter& painter)
+    -> void
+{
+    int generation_count = s.buf.size();
+
+    if (generation_count == 0)
+        return;
+
+    using CoordMap = plot::LinearCoordinateMapping<double, int>;
+    using Axis = plot::Axis<CoordMap>;
+    using Axes = plot::Axes2d<Axis, Axis>;
+
+    auto metric = [&](size_t index) -> const std::vector<double>&
+    {
+        return s.buf[index].*(d.metric);
+    };
+
+    auto state_count = metric(generation_count-1).size();
+
+    auto y_max = [&]{
+        auto gen_range = common::index_range<size_t>(generation_count);
+        return std::accumulate(
+            gen_range.begin(), gen_range.end(), 0.,
+            [&](double acc, size_t generation){
+                const auto& m = metric(generation);
+                auto n = std::min(m.size(), state_count);
+                auto x = *std::max_element(m.begin(), m.begin()+n);
+                return std::max(acc, x);
+            });
+        }();
+
+    auto axes_painter = plot::AxesPainter{
+        Axes{
+            .x = {
+                .mapping{
+                    .from{
+                        .begin = -static_cast<double>(s.buf_size),
+                        .end = 0}},
+                .label = d.x_label },
+            .y = {
+                .mapping{ .from = { .begin = 0., .end = y_max }},
+                .label = d.y_label },
+            .title = d.title
+        },
+        rect
+    };
+    const auto& axes = axes_painter.axes();
+
+    auto rc = axes_painter.layout().rect(layout::central);
+
+    auto& pal = ensure_palette(
+        s.*(d.palette),
+        state_count,
+        d.default_palette_color,
+        d.strict_palette_match);
+
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    for (size_t state=0; state<state_count; ++state)
+    {
+        auto path = QPainterPath{};
+        auto path_started = false;
+        for (int generation=0; generation<generation_count; ++generation)
+        {
+            const auto& m = metric(generation);
+            if (m.size() <= state)
+                continue;
+            auto px = axes.x.mapping(generation - generation_count);
+            auto py = axes.y.mapping(m[state]);
+            if (path_started)
+                path.lineTo(px, py);
+            else
+            {
+                path.moveTo(px, py);
+                path_started = true;
+            }
+        }
+        if (!path.isEmpty())
+        {
+            constexpr int pen_width = 2;
+            auto pen_color = gc_visual::qcolor(map_color(pal, state));
+            auto pen_style = Qt::SolidLine;
+            auto lightness = pen_color.lightnessF();
+            constexpr auto lightness_threshold = 0.9;   // TODO better
+            if (lightness > lightness_threshold)
+            {
+                lightness /= 2;
+                pen_style = Qt::DashLine;
+                pen_color = QColor::fromHslF(
+                    pen_color.hslHue(), pen_color.hslSaturationF(), lightness);
+            }
+            auto pen =
+                QPen(pen_color, pen_width, pen_style);
+            painter.strokePath(path, pen);
+        }
+    }
+
+    axes_painter.draw(painter);
+}
+
+auto plot_metric(ImageMetricsView::Storage& s,
+                 const QRect& rect,
+                 QPainter& painter)
+    -> void
+{
+    auto d = metric_view_data(s.type);
+    switch (d.plot_mode)
+    {
+    case PlotMode::HistogramHistory:
+        plot_histogram_metric(d, s, rect, painter);
+        break;
+    case PlotMode::TimeSeries:
+        plot_time_series_metric(d, s, rect, painter);
+        break;
+    }
 }
 
 } // anonymous namespace
