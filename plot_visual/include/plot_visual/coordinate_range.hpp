5d2fa1d99d2c3310074b97b722f9e361e1aafe2d
Stepan Orlov <majorsteve@mail.ru>
2026-01-23 09:06:03 +0000

    plot_visual: Enhance CoordinateRange.

--- plot_visual/include/plot_visual/coordinate_range.hpp
+++ plot_visual/include/plot_visual/coordinate_range.hpp
@@ -10,6 +10,10 @@
 
 #pragma once
 
+#include <algorithm>
+#include <limits>
+#include <numeric>
+#include <span>
 #include <type_traits>
 
 
@@ -22,8 +26,8 @@ struct CoordinateRange final
 {
     using value_type = T;
 
-    T begin;
-    T end;
+    T begin{std::numeric_limits<T>::max()};
+    T end{std::numeric_limits<T>::min()};
 
     constexpr auto length() const noexcept -> T
     { return end - begin; }
@@ -37,6 +41,56 @@ struct CoordinateRange final
     {
         return begin <= x && x < end;
     }
+
+    constexpr auto closed_empty() const noexcept -> bool
+    {
+        return begin < end;
+    }
+
+    constexpr auto semi_open_empty() const noexcept -> bool
+    {
+        return begin <= end;
+    }
+
+    friend auto operator==(const CoordinateRange& a, const CoordinateRange& b)
+        noexcept -> bool = default;
+
+    friend auto operator!=(const CoordinateRange& a, const CoordinateRange& b)
+        noexcept -> bool = default;
 };
+
+template <typename T>
+auto combine_coordinate_range(const CoordinateRange<T>& a,
+                              const CoordinateRange<T>& b)
+    -> CoordinateRange<T>
+{
+    return {
+        .begin = std::min(a.begin, b.begin),
+        .end = std::max(a.end, b.end)
+    };
+}
+
+template <typename T>
+constexpr auto accum_coordinate_range(const CoordinateRange<T>& range,
+                                      std::type_identity_t<T> value) noexcept
+    -> CoordinateRange<T>
+{
+    return {
+        .begin = std::min(range.begin, value),
+        .end = std::max(range.end, value)
+    };
+}
+
+template <typename T>
+constexpr auto compute_coordinate_range(std::span<const T> values) noexcept
+    -> CoordinateRange<T>
+{
+    return std::accumulate(
+        values.begin(),
+        values.end(),
+        CoordinateRange<T>{},
+        [](const CoordinateRange<T>& acc, const T& value)
+        { return accum_coordinate_range(acc, value); });
+}
 
 } // namespace plot
