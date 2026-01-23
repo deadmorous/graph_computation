a62dc90cac2b559ba2fd2036382add5f00ba74e5
Stepan Orlov <majorsteve@mail.ru>
2026-01-23 00:04:54 +0000

    plot_visual: Add a test for LiveTimeSeries.

---
+++ plot_visual/test/test_live_time_series.cpp
@@ -0,0 +1,69 @@
+/** @file
+ * @brief TODO: Brief docstring.
+ *
+ * TODO: More documentation here
+ *
+ * Copyright (C) 2026 MPK Software, St.-Petersburg, Russia
+ *
+ * @author Stepan Orlov <majorsteve@mail.ru>
+ */
+
+#include "plot_visual/live_time_series.hpp"
+
+#include <common/index_range.hpp>
+
+#include <gtest/gtest.h>
+
+#include <numeric>
+
+
+namespace {
+
+using dvec = std::vector<double>;
+
+auto test_vec(size_t index, size_t size) -> dvec
+{
+    auto result = dvec(size);
+    std::iota(result.begin(), result.end(), index*size);
+    return result;
+}
+
+} // anonymous namespace
+
+TEST(PlotVisual, LiveTimeSeries_Basic)
+{
+    constexpr size_t capacity = 5;
+    constexpr size_t values_per_frame = 3;
+
+    auto ts = plot::LiveTimeSeries{};
+    ts.set_frame_capacity(capacity);
+
+    EXPECT_EQ(ts.frame_capacity(), capacity);
+
+    EXPECT_TRUE(ts.frames().empty());
+
+    for (auto index : common::index_range<size_t>(2*capacity))
+    {
+        auto v = test_vec(index, values_per_frame);
+        ts.add(v);
+
+        auto expected_frame_count = std::min(1+index, capacity);
+        auto frames = ts.frames();
+        EXPECT_FALSE(frames.empty());
+        EXPECT_EQ(frames.size(), expected_frame_count);
+
+        size_t first_frame_ordinal =
+            index < capacity ? 0 : index - capacity + 1;
+
+        EXPECT_EQ(frames.front().ordinal, first_frame_ordinal);
+        EXPECT_EQ(frames.back().ordinal, index);
+
+        for (size_t ordinal = first_frame_ordinal; const auto& frame : frames)
+        {
+            EXPECT_EQ(frame.ordinal, ordinal);
+            auto expected_values = test_vec(ordinal, values_per_frame);
+            EXPECT_PRED2(std::ranges::equal, frame.values, expected_values);
+            ++ordinal;
+        }
+    }
+}
